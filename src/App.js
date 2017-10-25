2483e55b70cc213ef8f63940d865740faed55ce2
snopeks <creativesnopeks@gmail.com>
2017-10-24 22:27:12 -0700

    add jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from './Jumbotron';
 
 class App extends Component {
   render() {
@@ -8,10 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Language App</h1>
         </header>
+        <Jumbotron/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Amazing Language App
         </p>
       </div>
     );
---
+++ src/Jumbotron.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Jumbotron extends Component{
+  render(){
+    return(
+      <div>
+          <p>Jumbotron goes here!</p>
+
+      </div>
+    )
+  }
+}
+
+export default Jumbotron
